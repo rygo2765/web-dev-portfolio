dc593700c3cff26091c127706a3a0140e57aeb52
Ryan Goh <ryangohce@gmail.com>
2024-08-15 14:34:43 +0800

    stair animation wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 //components
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
+        <StairTransition/>
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
---
+++ components/Stairs.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+//variants
+const stairAnimation = {
+    initial: {
+        top: '0%',
+    },
+    animate: {
+        top: '100%',
+    }
+}
+
+const Stairs: React.FC = () => {
+  return (
+    <div>Stairs</div>
+  )
+}
+
+export default Stairs
