5d846182357f9ab4a65ea9fe79be30a4b00812c0
Ryan Goh <ryangohce@gmail.com>
2024-08-22 16:03:12 +0800

    complete mobile nav implementation, homepage wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <main>homepage</main>;
-}
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home: React.FC = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div>text</div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
