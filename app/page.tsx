c72395207cf19b765b0b081fae3917c1b1cc35fd
Ryan Goh <ryangohce@gmail.com>
2024-08-27 12:46:57 +0800

    complete implementing homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,15 @@ import { FiDownload } from "react-icons/fi";
 //components
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home: React.FC = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          {/* text */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1">
               Hello I'm <br /> <span className="text-accent">Ryan Goh</span>
@@ -39,11 +41,13 @@ const Home: React.FC = () => {
               </div>
             </div>
           </div>
-          <div>
-            <Photo/>
+          {/* photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
           </div>
         </div>
       </div>
+      <Stats/>
     </section>
   );
 };
